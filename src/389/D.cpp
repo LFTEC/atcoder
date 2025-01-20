2048df67e4a4993f4dfb8562b7bcaca80321ae62
姜耀 <ybjy82@163.com>
2025-01-20 20:48:16 +0800

    date: 2025-1-20 增加389D的解析

--- src/389/D.cpp
+++ src/389/D.cpp
@@ -1,1 +1,28 @@
-#include <bits/>
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+int main()
+{
+	ll r;
+	cin >> r;
+
+	auto f = [&](ll a, ll b) {
+		return (2 * a + 1) * (2 * a + 1) + (2 * b + 1) * (2 * b + 1) <= 4 * r * r;
+
+		};
+
+	ll ans = 0;
+	ll u = r - 1;
+	for (int i = 1; f(i, 1); i++)
+	{
+		while (!f(i, u)) u--;
+		ans += u;
+	}
+
+	ans *= 4;
+	ans += (r - 1) * 4 + 1;
+	cout << ans;
+
+	return 0;
+}
