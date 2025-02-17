c744d18b985fb21c2ec3734fd1c671ada35f0f83
chris <jcpuff20@gmail.com>
2025-02-17 17:01:32 +0800

    date: 2025-2-17 393_D

--- src/393/D.cpp
+++ src/393/D.cpp
@@ -2,12 +2,33 @@
 using namespace std;
 
 
-struct edge
-{
-	int u, v, weight;
-};
 int main()
 {
+	int n;
+	cin >> n;
+	int c1 = 0;
+	vector<int> d;
+	d.push_back(0);
+	while (n--)
+	{
+		char i;
+		cin >> i;
+		if (i == '0') 
+		{
+			d.push_back(c1);
+		}
+		else
+		{
+			c1++;
+		}
+	}
 
+	long long ans = 0;
+	for (int i = 1; i < d.size(); i++)
+	{
+		ans += min(d[i], c1 - d[i]);
+	}
+
+	cout << ans;
 	return 0;
 }
